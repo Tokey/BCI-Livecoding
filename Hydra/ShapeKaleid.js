31964509c55755281a61f01a41e4cff5b029ad21
Samin Shahriar Tokey <saminshahriar@rocketmail.com>
2022-11-28 15:36:54 -0500

    Update ShapeKaleid.js

--- Hydra/ShapeKaleid.js
+++ Hydra/ShapeKaleid.js
@@ -1,5 +1,4 @@
-shape([3,4,5,6,5,4,3].ease('easeInOutCubic').fast(5),1,0)
-
-.kaleid(2)
-.repeatX(3.0, 0.0)
+shape([3,4,5,6,5,4,3,90].ease('easeInOutCubic').fast(5),0.4,0)
+.repeatX(6.0)
+.repeatY(5.0)
 .out(o0)
